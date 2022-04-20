19edb3095c5bdc7e5d9c5f87462b62539a4a98c0
Aman Yadav <42231378+yadav-aman@users.noreply.github.com>
2022-04-20 15:51:13 +0530

    Moved analytics page component to own directory

--- pages/collections/[name]/analytics/[linkAlias].tsx
+++ pages/collections/[name]/analytics/[linkAlias].tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { LayoutPage } from '../../../../types/ui';
-import AnalyticsPage from '../../../../components/AnalyticsPage';
+import AnalyticsPage from '../../../../components/analytics/AnalyticsPage';
 
 const AnalyticsItemPage: LayoutPage = () => {
   const { linkAlias } = useRouter().query as any;
